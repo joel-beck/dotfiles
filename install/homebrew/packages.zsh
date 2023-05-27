8c4cb7afdd2a1b66337392e29fe248ea48a58dfe
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-05-28 00:01:10 +0200

    added homebrew packages

--- install/homebrew/packages.zsh
+++ install/homebrew/packages.zsh
@@ -1,10 +1,10 @@
 # Install Homebrew Packages, keep the list in alphabetical order
 
-# Bourne-Again SHell, a UNIX command interpreter
-brew install bash
-
 # Bash scripts that integrate bat with various command-line tools
 brew install bat-extras
+
+# Bourne-Again SHell, a UNIX command interpreter
+brew install bash
 
 # New way to see and navigate directory trees
 brew install broot
@@ -15,6 +15,9 @@ brew install chafa
 # Cross-platform make
 brew install cmake
 
+# Transfer data with URL syntax support via command line
+brew install curl
+
 # Utility that creates projects from templates
 brew install cookiecutter
 
@@ -24,10 +27,10 @@ brew install cpanminus
 # Pack, ship and run any application as a lightweight container
 brew install docker
 
-# Isolated development environments using Docker
+# Compose and run multi-container Docker applications
 brew install docker-compose
 
-# More intuitive version of du in rust
+# More intuitive version of 'du' in rust
 brew install dust
 
 # Select default apps for documents and URL schemes on macOS
@@ -36,7 +39,7 @@ brew install duti
 # Modern replacement for 'ls'
 brew install exa
 
-# Simple, fast and user-friendly alternative to find
+# Simple, fast and user-friendly alternative to 'find'
 brew install fd
 
 # Play, record, convert, and stream audio and video
@@ -75,20 +78,17 @@ brew install perl
 # General-purpose scripting language
 brew install php
 
-# 📦🚀 Fast, disk space efficient package manager
+# Fast, disk space efficient package manager
 brew install pnpm
 
-# A Zsh Theme
+# Zsh Theme
 brew install powerlevel10k
 
 # Framework for managing multi-language pre-commit hooks
 brew install pre-commit
 
-# Seamless operability between C++11 and Python
-brew install pybind11
-
-# Python version management & Install latest Python version
-brew install pyenv && pyenv install $(pyenv install --list | grep -v - | grep -v b | tail -1)
+# Code searching tool similar to ack, but faster
+brew install ripgrep
 
 # Powerful, clean, object-oriented scripting language
 brew install ruby
@@ -96,7 +96,7 @@ brew install ruby
 # Static analysis and lint tool, for (ba)sh scripts
 brew install shellcheck
 
-# Command-line interface for https://speedtest.net bandwidth tests
+# Command-line interface for speedtest.net bandwidth tests
 brew install speedtest-cli
 
 # Cross-shell prompt for astronauts
@@ -105,7 +105,7 @@ brew install starship
 # Organize software neatly under a single directory tree (e.g. /usr/local)
 brew install stow
 
-# Programmatically correct mistyped console commands
+# Seamlessly correct mistyped console commands
 brew install thefuck
 
 # CLI tool that moves files or folder to the trash
@@ -114,11 +114,32 @@ brew install trash
 # Display directories as trees (with optional color/HTML output)
 brew install tree
 
+# Line-oriented search tool
+brew install the_silver_searcher
+
+# Simplified and community-driven man pages
+brew install tldr
+
 # Language for application scale JavaScript development
 brew install typescript
+
+# Modern build tool for JavaScript and CSS
+brew install vite
 
 # Extraction utility for .zip compressed archives
 brew install unzip
 
 # A youtube-dl fork with additional features and fixes
 brew install yt-dlp
+
+# Command language interpreter
+brew install zsh
+
+# Python version management & Install latest Python version
+brew install pyenv && pyenv install $(pyenv install --list | grep -v - | grep -v b | tail -1)
+
+# Seamless operability between C++11 and Python
+brew install pybind11
+
+# In-memory database that persists on disk
+brew install sqlite
