99ce68652f472143086c389f7c8af31f544f7493
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-07-01 11:16:39 +0200

    updated path to icloud symlinkes folder

--- install/02_symlinks.zsh
+++ install/02_symlinks.zsh
@@ -4,4 +4,4 @@
 # - iCloud: Symbolic Link to iCloud Drive Folder, i.e. the *source* files live in iCloud
 
 # creates new `iCloud` folder automatically
-ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs" iCloud
+ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs" "$HOME/iCloud"
