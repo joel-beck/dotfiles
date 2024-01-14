56712ecdd63f43411c7b9c266a02ac18573a0c70
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2024-01-14 21:49:03 +0100

    add rich logging config

---
+++ not-synced/python/logging_rich.py
@@ -0,0 +1,40 @@
+"""
+Logging Setup with rich
+"""
+
+import logging
+
+from rich.console import Console
+from rich.highlighter import ReprHighlighter
+from rich.logging import RichHandler
+from rich.theme import Theme
+
+theme = Theme({
+    "info": "cyan",
+    "warning": "yellow",
+    "error": "bold red",
+    "critical": "bold red",
+})
+
+handler = RichHandler(
+    console=Console(theme=theme),
+    show_time=True,
+    omit_repeated_times=True,
+    show_level=True,
+    show_path=True,
+    enable_link_path=True,
+    markup=False,  # if true, the filename and line number are not displayed
+    highlighter=ReprHighlighter(),
+    rich_tracebacks=True,
+)
+
+formatter = logging.Formatter(
+    # if function name in included, hyperlinks do not work
+    fmt="%(message)s [%(filename)s:%(lineno)d]",
+    datefmt="[%Y-%m-%d %H:%M:%S]",
+)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("rich")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
